00345a5f5c8cfa77f69606071dccfc3a6bc6c348
Matthew Gabeler-Lee <cheetah@fastcat.org>
2016-12-20 01:15:37 -0500

    Deal with twitter escape format

--- feed.php
+++ feed.php
@@ -5,12 +5,14 @@ if (isset($q)){
 	$td = $twitter_data;
 }
 $arrLen = count($td);
-//print(PHP_EOL.'<!--'.$arrLen.'-->'.PHP_EOL);
+//print(PHP_EOL.'<!--'.$json.'-->'.PHP_EOL);
 for ($i=0; $i<$arrLen; $i++) {
+	// twitter encodes text ... so we need to decode and reencode for non-cdata uses
+	$cleanText=htmlspecialchars_decode($td[$i]['text']);
 	print(PHP_EOL. '	<entry>'. PHP_EOL);
 		print('		<id>tag:twitter.com,' . date("Y-m-d", strtotime($td[$i]['created_at'])) . ':/' . $td[$i]['user']['screen_name'] . '/statuses/' . $td[$i]['id_str'] . '</id>'. PHP_EOL);
 		print('		<link href="https://twitter.com/'.$td[$i]['user']['screen_name'].'/statuses/'. $td[$i]['id_str'] .'" rel="alternate" type="text/html"/>'. PHP_EOL);
-		print('		<title>'.$td[$i]['user']['screen_name'].': '.htmlspecialchars($td[$i]['text']).'</title>'. PHP_EOL);
+		print('		<title type="html">'.htmlspecialchars($td[$i]['user']['screen_name'].': '.$cleanText).'</title>'. PHP_EOL);
 		print('		<summary type="html"><![CDATA['.$td[$i]['user']['screen_name'].': '.$td[$i]['text'].']]></summary>'. PHP_EOL);
 
 		$feedContent = '		<content type="html"><![CDATA[<p>'.nl2br($td[$i]['text']).'</p>]]></content>';
